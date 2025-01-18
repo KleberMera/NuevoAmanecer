4508a054c5c7c9d0a63e1aa563af6f28b68a2964
KleberMera <klebermera2016@gmail.com>
2025-01-18 10:52:17 -0500

    tab mobile version

---
+++ src/app/layout/components/tab/tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-tab',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './tab.component.html',
+  styleUrl: './tab.component.scss'
+})
+export class TabComponent {
+
+}
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from "./components/header/header.component";
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { SidebarService } from '../services/sidebar.service';
+import { TabComponent } from "./components/tab/tab.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent, RouterOutlet, SidebarComponent],
+  imports: [HeaderComponent, RouterOutlet, SidebarComponent, TabComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
